7c4aaa06dc65fc10c837ff39572344afd35e94c6
dari-kayoo <78098445+dari-kayoo@users.noreply.github.com>
2021-11-05 20:36:37 +0600

    Update lab6.sql

--- lab6/lab6.sql
+++ lab6/lab6.sql
@@ -23,36 +23,36 @@ INNER JOIN client c ON c.city = d.location;
 -- 1d
 SELECT s.id, s.amount, c.name, c.city
 FROM sell s
-INNER JOIN client c on s.client_id = c.id
-WHERE s.amount >= 100 and s.amount <= 500;
+INNER JOIN client c ON s.client_id = c.id
+WHERE s.amount >= 100 AND s.amount <= 500;
 
 -- 1e
 SELECT *
 FROM dealer d
-LEFT JOIN client c on d.location = c.city;
+LEFT JOIN client c ON d.location = c.city;
 
 --1f
 SELECT c.name, c.city, d.name, d.charge
 FROM client c
-JOIN dealer d on c.dealer_id = d.id;
+JOIN dealer d ON c.dealer_id = d.id;
 
 -- 1g
 SELECT c.name, c.city, d
 FROM  client c
-JOIN dealer d on dealer_id = d.id
+JOIN dealer d ON dealer_id = d.id
 WHERE d.charge > 0.12;
 
 -- 1h
 SELECT c.name, c.city, s.id, s.date, s.amount, d.name,d.charge
 FROM client c
 JOIN dealer d ON c.dealer_id = d.id
-JOIN sell s on c.id = s.client_id;
+JOIN sell s ON c.id = s.client_id;
 
 -- 1i
 SELECT c.name, c.priority,d.name, s.id, s.amount
 FROM dealer d
-LEFT JOIN client c on d.id = c.dealer_id
-LEFT JOIN sell s on c.id = s.client_id
+LEFT JOIN client c ON d.id = c.dealer_id
+LEFT JOIN sell s ON c.id = s.client_id
 WHERE s.amount >= 2000 AND c.priority IS NOT NULL;
 
 -- 2a
@@ -105,7 +105,7 @@ GROUP BY c.city;
 
 --2g
 CREATE VIEW g2 AS
-SELECT c.city, SUM(s.amount * (d.charge + 1)) as totalexpense, SUM(s.amount) as totalamount
+SELECT c.city, SUM(s.amount * (d.charge + 1)) AS totalexpense, SUM(s.amount) AS totalamount
 FROM client c
 JOIN sell s ON c.id = s.client_id
 JOIN dealer d ON s.dealer_id = d.id and c.city = d.location
