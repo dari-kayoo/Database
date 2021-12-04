09e555d427d9fdd4b7855a0c10f9ca38592eca73
dari-kayoo <78098445+dari-kayoo@users.noreply.github.com>
2021-12-04 23:42:38 +0600

    Update 4_Queries_selecting.sql

--- Project/4_Queries_selecting.sql
+++ Project/4_Queries_selecting.sql
@@ -13,7 +13,7 @@ SELECT vehicles.VIN, person.full_name
 FROM person, vehicles, manufacturer,suppliers,part
 WHERE vehicles.customer_id = person.id AND manufacturer.VIN=vehicles.VIN AND suppliers.name = 'Getrag'
 AND part.made_date BETWEEN '2014-02-06' AND '2014-03-05';
-
+----
 SELECT brand_name, SUM(s.price) as sum
 FROM models, sales s
 INNER JOIN vehicles v
@@ -27,7 +27,7 @@ ON models.brand_name = brands.name
 Group By brand_name
 Order By sum DESC
 LIMIT 2;
-
+-----
 SELECT brand_name, COUNT(m.brand_name)
 FROM sales s
 INNER JOIN vehicles
@@ -42,12 +42,11 @@ ON m.brand_name = b.name
 Group By m.brand_name
 Order By COUNT(m.brand_name) DESC
 LIMIT 2;
-
+-----
 SELECT date_part('month', (s.date::date)) = 2020, COUNT(m.brand_name)
 FROM sales s
 INNER JOIN vehicles v
 ON s.VIN = v.VIN
-
 INNER JOIN options o
 ON o.id = v.option_id
 INNER JOIN models m
@@ -58,3 +57,14 @@ WHERE b.name = 'convertibles' and date_part('year', (s.date::date)) = 2020
 Group By m.brand_name
 Order By COUNT(m.brand_name) DESC
 LIMIT 1;
+------
+SELECT dealers.name, MAX(date_part('day', (sales.date::date)))
+FROM dealers
+INNER JOIN sales
+ON sales.id = dealers.id
+INNER JOIN inventory
+ON dealers.id = inventory.id
+INNER JOIN vehicles
+ON sales.VIN = vehicles.VIN
+Group By dealers.name;
+
